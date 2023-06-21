5408fb7365f5db673bf7366b7a573aff4045969f
jankaplavka <plavcakova@gmail.com>
2023-06-21 09:09:30 +0200

    treti den, oprava animacie, normalny, horuci a prehriaty reaktor

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -9,16 +9,61 @@ public class Reactor extends AbstractActor {
     private int damage;
     private Animation normalAnimation;
     public Reactor(){
-        temperature= 0;
-        state = false;
-        damage = 0;
-        normalAnimation = new Animation(
+        this.temperature= 0;
+        this.state = false;
+        this.damage = 0;
+        this.normalAnimation = new Animation(
             "sprites/reactor_on.png",
             80, 80, 0.1F,
             Animation.PlayMode.LOOP_PINGPONG
         );
-        setAnimation(normalAnimation);
+        setAnimation(this.normalAnimation);
     }
+    public int getTemperature(){
+       return this.temperature;
+    }
+    public int getDamage(){
+        return this.damage;
+    }
+
+    public void increaseTemperature(int increment){
+        this.temperature = this.temperature + increment;
+
+//
+        if(this.temperature>=6000) {
+            Animation animation = new Animation(
+                "sprites/reactor_broken.png",
+                80, 80,0.1F,
+                Animation.PlayMode.LOOP_PINGPONG);
+            setAnimation(animation);
+        }
+        else
+    //        animacia prehrievajuci reaktor
+            if(this.temperature>=4000){
+                Animation animation = new Animation(
+                    "sprites/reactor_hot.png",
+                    80, 80,0.1F,
+                    Animation.PlayMode.LOOP_PINGPONG);
+                setAnimation(animation);
+            }
+            else
+        //        normalna animacia
+                if(this.temperature<2000){
+                    Animation animation = new Animation(
+                        "sprites/reactor_on.png",
+                        80, 80,0.1F,
+                        Animation.PlayMode.LOOP_PINGPONG);
+                    setAnimation(animation);
+                }
+
+
+
+
+    }
+
+
+
+
 
 
 
