7e22014f8639b01f6e1bc4aad3b5bfde220f13d5
jankaplavka <plavcakova@gmail.com>
2023-06-21 14:01:07 +0200

    treti den, vytvorenie kladiva a pouzitia kladiva a aktualizacia animacie

---
+++ src/main/java/sk/tuke/kpi/oop/game/Hammer.java
@@ -0,0 +1,30 @@
+package sk.tuke.kpi.oop.game;
+
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+public class Hammer extends AbstractActor {
+    private int usages;
+
+    //
+//konstruktor
+    public Hammer() {
+        this.usages = 1;
+        Animation animation = new Animation("sprites/hammer.png");
+        setAnimation(animation);
+    }
+
+    public int getUsages() {
+        return (this.usages);
+    }
+
+//metoda na puzitie kladiva
+public void use(){
+        this.usages--;
+        if (this.usages==0){
+            getScene().removeActor(this);
+    }
+
+}
+
+}
--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -8,67 +8,151 @@ public class Reactor extends AbstractActor {
     private boolean state;
     private int damage;
     private Animation normalAnimation;
-    public Reactor(){
-        this.temperature= 0;
+    private Animation hotAnimation;
+    private Animation brokenAnimation;
+
+    public Reactor() {
+        this.temperature = 0;
         this.state = false;
         this.damage = 0;
         this.normalAnimation = new Animation(
             "sprites/reactor_on.png",
             80, 80, 0.1F,
-            Animation.PlayMode.LOOP_PINGPONG
-        );
+            Animation.PlayMode.LOOP_PINGPONG);
+
+        this.hotAnimation = new Animation(
+            "sprites/reactor_hot.png",
+            80, 80, 0.1F,
+            Animation.PlayMode.LOOP_PINGPONG);
+
+
+        this.brokenAnimation = new Animation(
+            "sprites/reactor_broken.png",
+            80, 80, 0.1F,
+            Animation.PlayMode.LOOP_PINGPONG);
+        //nastavenie animacie na normalny reaktor
         setAnimation(this.normalAnimation);
     }
-    public int getTemperature(){
-       return this.temperature;
+
+    public int getTemperature() {
+        return this.temperature;
     }
-    public int getDamage(){
+
+    public int getDamage() {
         return this.damage;
     }
 
-    public void increaseTemperature(int increment){
-        this.temperature = this.temperature + increment;
-
-//
-        if(this.temperature>=6000) {
-            Animation animation = new Animation(
-                "sprites/reactor_broken.png",
-                80, 80,0.1F,
-                Animation.PlayMode.LOOP_PINGPONG);
-            setAnimation(animation);
+    public void increaseTemperature(int increment) {
+        if (increment < 0) {
+            return;
         }
-        else
-    //        animacia prehrievajuci reaktor
-            if(this.temperature>=4000){
-                Animation animation = new Animation(
-                    "sprites/reactor_hot.png",
-                    80, 80,0.1F,
-                    Animation.PlayMode.LOOP_PINGPONG);
-                setAnimation(animation);
-            }
-            else
-        //        normalna animacia
-                if(this.temperature<2000){
-                    Animation animation = new Animation(
-                        "sprites/reactor_on.png",
-                        80, 80,0.1F,
-                        Animation.PlayMode.LOOP_PINGPONG);
-                    setAnimation(animation);
+        this.temperature = this.temperature + increment;
+        if (this.damage == 100) {
+            return;
+        }
+        updateAnimation();
+
+//      animacia vybuchujuci reaktor
+//        if (this.temperature >= 6000) {
+//            setAnimation(this.brokenAnimation);
+//        } else
+//            //      animacia prehrievajuci reaktor
+//            if (this.temperature >= 4000) {
+//                setAnimation(this.hotAnimation);
+//            } else
+//                //      normalna animacia
+//                if (this.temperature < 2000) {
+//                    setAnimation(this.normalAnimation);
+//                }
+
+//     nastavevanie damageu
+        if (this.temperature > 2000) {
+            if (this.temperature > 6000) {
+                this.damage = 100;
+            } else {
+                int damage = (this.temperature / 40) - 50;
+                if (damage > this.damage) {
+                    this.damage = damage;
                 }
+            }
+        }
+
+    }
 
 
+    public void decreaseTempreature(int decrement) {
+        if (decrement < 0) {
+            return;
+        }
+        this.temperature = this.temperature - decrement;
+        if (this.damage == 100) {
+            return;
+        }
+        updateAnimation();
 
 
     }
 
 
+    //nova metoda na aktualizaciu animacie
+    public void updateAnimation() {
+        if (this.temperature >= 6000) {
+            setAnimation(this.brokenAnimation);
+        } else
+            //      animacia prehrievajuci reaktor
+            if (this.temperature >= 4000) {
+                setAnimation(this.hotAnimation);
+            } else
+                //      normalna animacia
+                if (this.temperature < 4000) {
+
+                    setAnimation(this.normalAnimation);
+                }
+
+    }
+
+    //metoda na opravenie reaktora pomocou kladiva
+    public void repairWith(Hammer hammer) {
+        if (hammer == null) {
+            return;
+        }
 
 
-
-
-
-
-
+//        if (this.damage>0) {
+//            hammer.use();
+////            hammer.getUsages();
+//            if (this.damage == 100) {
+//                return;
+//            } else {
+//                if (this.damage > 50) {
+//                    this.damage = this.damage - 50;
+//                } else this.damage = 0;
+//            }
+//        }
+        if (this.damage == 0 || this.damage == 100) {
+            return;
+        }
+        if (this.damage < 50) {
+            this.damage = 0;
+            this.temperature = 0;
+            updateAnimation();
+        } else {
+            this.damage = this.damage - 50;
+            this.temperature = 2000;
+            updateAnimation();
+        }
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
